5a1cf0e9c8d7f139ba89e8bd6d0f81bfbadeb032
dxoakes <donald.oakes@centurylink.com>
2017-06-06 19:46:36 -0600

    Issue #114

--- src/core/plugins/swagger-js/index.js
+++ src/core/plugins/swagger-js/index.js
@@ -3,12 +3,16 @@ import Swagger from "swagger-client"
 module.exports = function({ configs }) {
   return {
     fn: {
-      fetch: Swagger.makeHttp(configs.preFetch, configs.postFetch),
+      fetch: function(obj) {
+        // replace 'undefined' segments in url
+        obj.url = obj.url.replace(/\/undefined/g, '');
+        return Swagger.makeHttp(configs.preFetch, configs.postFetch)(obj);
+      },
       buildRequest: Swagger.buildRequest,
       execute: Swagger.execute,
       resolve: Swagger.resolve,
       serializeRes: Swagger.serializeRes,
-      opId: Swagger.helpers.opId
+      opId: Swagger.helpers.opId      
     }
   }
 }
